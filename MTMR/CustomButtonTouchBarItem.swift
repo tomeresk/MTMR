1621a10e44c1eb01814396cc6bca64c59d1b4554
Tomer Eskenazi <tomeresk@gmail.com>
2019-08-04 18:11:41 +0300

    change font size

--- MTMR/CustomButtonTouchBarItem.swift
+++ MTMR/CustomButtonTouchBarItem.swift
@@ -333,7 +333,7 @@ class LongPressGestureRecognizer: NSPressGestureRecognizer {
 
 extension String {
     var defaultTouchbarAttributedString: NSAttributedString {
-        let attrTitle = NSMutableAttributedString(string: self, attributes: [.foregroundColor: NSColor.white, .font: NSFont.systemFont(ofSize: 15, weight: .regular), .baselineOffset: 1])
+        let attrTitle = NSMutableAttributedString(string: self, attributes: [.foregroundColor: NSColor.white, .font: NSFont.systemFont(ofSize: 18, weight: .regular), .baselineOffset: 1])
         attrTitle.setAlignment(.center, range: NSRange(location: 0, length: count))
         return attrTitle
     }
